54d3c0d69c9fe795b941707b24830763c924cae4
Alex Schuy <alexjschuy@gmail.com>
2022-03-01 10:02:39 -0800

    Fix seeds.

---
+++ generate_new.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+./generateEvents.sh mumu_nunuww/INT_S0_12.txt
+./generateEvents.sh mumu_nunuww/QUAD_S0_12.txt
+./generateEvents.sh mumu_nunuww/INT_S1_12.txt
+./generateEvents.sh mumu_nunuww/QUAD_S1_12.txt
+./generateEvents.sh mumu_nunuww/INT_T0_12.txt
+./generateEvents.sh mumu_nunuww/QUAD_T0_12.txt
+./generateEvents.sh mumu_nunuww/INT_T2_12.txt
+./generateEvents.sh mumu_nunuww/QUAD_T2_12.txt
+./generateEvents.sh mumu_nunuww/INT_M0_12.txt
+./generateEvents.sh mumu_nunuww/QUAD_M0_12.txt
+./generateEvents.sh mumu_nunuww/INT_M7_12.txt
+./generateEvents.sh mumu_nunuww/QUAD_M7_12.txt
+# ./generateEvents.sh mumu_nunuww/INT_M6_12.txt
+# ./generateEvents.sh mumu_nunuww/QUAD_M6_12.txt
